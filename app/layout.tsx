14f7a64c09d92ef22debd04de8dc1f63e3a2673d
Diego Neves <diegojcn@gmail.com>
2024-01-21 20:25:36 -0300

    refac: adding metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,26 @@
 import '@/app/ui/global.css'
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
 
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Dashapp',
+    default: 'Dashapp',
+  },
+  keywords: ["react", "next.js", "Diego Neves", "vercel"],
+  authors: [
+    {
+      name: "Diego Neves",
+      url: "https://www.linkedin.com/in/diego-jco-neves/"
+    }
+  ],
+  description: 'This is the study case of Diego Neves, brought to you by Vercel.',
+  metadataBase: new URL('https://github.com/diegojcn/nextjs-dashboard-app'),
+};
+export type Author = {
+  url?: string | URL;
+  name?: string;
+};
 export default function RootLayout({
   children,
 }: {
